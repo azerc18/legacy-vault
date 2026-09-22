a09082ea8643dc643829cc56601fd83498ba7215
azerc18 <loki18042006@gmail.com>
2026-09-22 20:43:06 +0700

    chore: initial project structure with spring boot framework

---
+++ src/main/java/com/ltld/app/legacyvault/LegacyVaultApplication.java
@@ -0,0 +1,13 @@
+package com.ltld.app.legacyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegacyVaultApplication.class, args);
+    }
+
+}
